e6f0c147e375a9d355c6031f2d25a590ebb3ab57
Saleem Shahzad <saleemwork123@gmail.com>
2025-02-24 11:07:21 +0500

    Word Counter
     It counts the no of words in your text

---
+++ Word Counter.py
@@ -0,0 +1,13 @@
+user_input=str(input("Enter your Text "))
+words=user_input.split()
+counter=0
+for i in words:
+    counter+=1
+print("The no of words in your text are: ",counter)
+
+
+
+
+
+
+    
